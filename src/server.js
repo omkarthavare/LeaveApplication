d25c6118ba8d10b5459b813435a8f44fe5e8ae4a
omkar <omkarthavare5@gmail.com>
2026-08-31 15:50:23 +0530

    databased connectivity done

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
+require("dotenv").config();
+
 const app = require('./app');
+const db = require("./config/db.js")
 
 const port = process.env.port || 5000;
 
-app.listen(port , ()=>{
-    console.log(`server is running on port  ${port}`);
-    console.log(`Local URL: http://localhost:${port}`);
-})
+async function startServer() {
+    try {
+        await db.query("select 1")
+        console.log("Mysql connected Successfully")
+
+        app.listen(port, () => {
+            console.log(`server is running on port  ${port}`);
+            console.log(`Local URL: http://localhost:${port}`);
+        })
+
+    } catch (error) {
+        console.error("Database connection failed", error.message)
+    }
+
+}
+
+startServer()
